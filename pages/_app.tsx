44794d282caec626a3806a93399afe8fd6a9e1c2
Rodney Jan Mandap <rodneyjan.mandap@gmail.com>
2025-12-10 23:44:23 +0000

    Remove unused Navigation component and SpeedInsights integration

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,4 @@
 import { Analytics } from "@vercel/analytics/react";
-import { SpeedInsights } from "@vercel/speed-insights/next";
 import "../styles/globals.css";
 
 import type { AppProps } from "next/app";
@@ -9,7 +8,6 @@ function MyApp({ Component, pageProps }: AppProps) {
     <>
       <Component {...pageProps} />
       <Analytics />
-      <SpeedInsights />
     </>
   );
 }
